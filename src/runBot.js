3b7c5ee9299bee603c8ae631e25df83edcfa3c12
João Carvalho <joao.antonio.carvalho27@gmail.com>
2023-04-26 12:09:16 -0300

    there was a error using .env file to import the openai_key, fixed when declare the string inside the rubBot.js

--- src/runBot.js
+++ src/runBot.js
@@ -1,10 +1,11 @@
 const { getOpenaiClient } = require('../connectToOpenAI/connect.js');
 const { generateChatResponse } = require('../requests/textQuestion.js');
-require('dotenv').config()
+
+const openai_key = "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" // your personal openai KEY
 
 const connectToOpenai = async function () {
   //connect with your api_key
-  const openai = await getOpenaiClient(process.env.OPENAI_KEY);
+  const openai = await getOpenaiClient(openai_key);
 
   return openai
 };
